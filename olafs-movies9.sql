c154533e98cd0ebeae956fa85796b41a4b1bfe9e
ozo7 <send2olaf@yahoo.de>
2019-11-06 12:49:03 +0100

    Create olafs-movies9.sql
    9 movies from my strange database

---
+++ olafs-movies9.sql
@@ -0,0 +1,10 @@
+INSERT INTO `movies` (`movie_id`, `title`, `description`, `picture`, `release_year`, `local_path`, `category_id`) VALUES
+(NULL, 'the bad and the good', 'bad bad bad bad bad bad bad bad bad', 'https://i0.wp.com/media.boingboing.net/wp-content/uploads/2016/07/gbu.jpg?w=800&ssl=1', 2020, 'c:\mymovies\olaf1.mp4', 2),
+(NULL, 'monster madness', 'very scary stuff', 'https://cinemassacre.com/wp-content/uploads/2016/09/26-Rocky-Horror-190x140.jpg', 1977, 'c:\mymovies\olaf2.mov', 2),
+(NULL, 'teletubbies', 'not only for kids but it is real shit', 'https://i.ytimg.com/vi/W7wQJyED91I/maxresdefault.jpg', 2010, 'c:\mymovies\olaf3.mov', 4),
+(NULL, 'deep dive horizon 99', 'dive into it? really?', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP-bb-STMcVVKBgMvs9pg4Hnjf4aGOuwGx4QyIOlDlZIWJXfP2wA&s', 1999, 'c:\mymovies\olaf4.mov', 4),
+(NULL, 'break your neck badly to the bones', 'it is not my fault!', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_SD4Fl4KSR9Oovr_soBpQZ8f1_rQ5Iapi2YwQHSSVUxDd6hy9mw&s', 2001, 'c:\mymovies\olaf5.mov', 4),
+(NULL, 'swampy swamp break up world, Gonzo Turtle will eat you all', 'boaaaahhhhrrrrrr', 'https://2.bp.blogspot.com/-UdoWFFJfhLA/W2jhhoc8NtI/AAAAAAAAYK8/MWLGOGeWdB092Aw39ALIt6Azntm1TTSRQCLcBGAs/s1600/Skanklodge.JPG', 2009, 'c:\mymovies\olaf6.mov', 2),
+(NULL, 'never thought that', 'it is an empty mind', 'https://i.ytimg.com/vi/euknE24COWA/maxresdefault.jpg', 2014, 'c:\mymovies\olaf7.mov', 3),
+(NULL, 'fun size', 'yes, just having fun', 'https://m.media-amazon.com/images/M/MV5BMjI1NDgyMjI4OV5BMl5BanBnXkFtZTcwNTg3MzM0OA@@._V1_.jpg', 2018, 'c:\mymovies\olaf8.mov', 1),
+(NULL, 'Passport', 'very serious stuff', 'https://i.vimeocdn.com/video/765578324.jpg?mw=1920&mh=1080&q=70', 1993, 'c:\mymovies\olaf9.mov', 2);
