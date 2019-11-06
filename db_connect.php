ecd6f5524d8ba088efcc4a0e15e51be9146bd298
R <55230013+rvw2019@users.noreply.github.com>
2019-11-06 15:36:49 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,6 +2,6 @@
 
     require_once 'database.php';
 
-    define('CONNECT', mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME));
+    $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
 
 ?>
