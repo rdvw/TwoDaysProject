52655ff8d7fb769ae178f1235c9f287415338d2a
R <55230013+rvw2019@users.noreply.github.com>
2019-11-07 15:56:40 +0100

    Update edit_category.php

--- edit_category.php
+++ edit_category.php
@@ -12,13 +12,24 @@
 
     <?php
     include_once('nav.html');
+
     require_once 'db_connect.php';
+
+    $title = '';
+    $description = '';
+    if (isset($_GET['id'])) {
+        $query = "SELECT * FROM categories WHERE category_id =" . $_GET['id'];
+        $result_query = mysqli_query($connect, $query);
+        $result = mysqli_fetch_assoc($result_query);
+        $title = $result['title'];
+        $description = $result['description'];
+    }
     ?>
 
     <form action="#" method="POST">
-        Title: <input type="text" name="title"><br>
-        Description: <input type="text" name="description"><br>
-        <input type="submit" name="submit" value="Add the category">
+        Title: <input type="text" name="title" value="<?php echo $title ?>" required><br>
+        Description: <input type="text" name="description" value="<?php echo $description ?>" required><br>
+        <input type="submit" name="submit" value="Edit the category">
     </form>
 
 </body>
@@ -30,51 +41,16 @@
 if (isset($_POST['submit'])) {
     $title = $_POST['title'];
     $description = $_POST['description'];
-    var_dump($_POST);
-    if (empty($title)) {
-        echo 'The title is missing. Please try again...';
-    } else {
-        if (empty($description)) {
-            echo 'The description is missing. Please try again...';
-        } else {
-            $query = "INSERT INTO categories(title, description) VALUES('$title', '$description')";
-            $result_query = mysqli_query($connect, $query);
-            if ($result_query)
-                echo 'The category was added successfully.';
-            else
-                echo 'Something went wrong. Please try again...';
-        }
+
+    if (isset($_GET['id'])) {
+        $query = "UPDATE categories SET title='$title', description='$description' WHERE category_id =" . $_GET['id'];
+        $result_query = mysqli_query($connect, $query);
+        if ($result_query)
+            echo 'The category was added successfully.';
+        else
+            echo 'Something went wrong. Please try again...';
     }
 }
-
-
-
-/*
-
-<?php
-    if (isset($_POST['submit'])) {
-        $title = $_POST['title'];
-        $description = $_POST['description'];
-        $picture = $_POST['picture'];
-        $release_year = $_POST['release_year'];
-        $local_path = $_POST['local_path'];
-        $category = $_POST['category'];
-        if(empty($title) || empty($description) || empty($picture) || empty($release_year) || empty($local_path) || empty($category)){
-            echo 'Missing same details. Try again...';
-        }else{
-            $query = "INSERT INTO movies(title, description, picture, release_year, local_path, category_id) VALUES('$title', '$description', '$picture', $release_year,'$local_path',$category)";
-            $result_query = mysqli_query($connect, $query);
-            if ($result_query)
-                echo 'Successfully registered new movie.';
-            else
-                echo 'Something went wrong. Try again...';
-        }
-    }
-
-    mysqli_close($connect);
-
-*/
-
 
 mysqli_close($connect);
 
