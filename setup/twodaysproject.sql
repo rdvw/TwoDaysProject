e5249120080fd2079fcdbe1a1d1b69c54ce5a643
R <55230013+rvw2019@users.noreply.github.com>
2019-11-08 09:12:38 +0100

    Update twodaysproject.sql

--- setup/twodaysproject.sql
+++ setup/twodaysproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 06, 2019 at 01:09 PM
+-- Generation Time: Nov 07, 2019 at 03:36 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -57,21 +57,49 @@ CREATE TABLE IF NOT EXISTS `actors_movies` (
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
-  `categtory_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(60) NOT NULL,
   `description` varchar(200) NOT NULL,
-  PRIMARY KEY (`categtory_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`categtory_id`, `title`, `description`) VALUES
-(1, 'Science Fiction', 'Science Fiction'),
+INSERT INTO `categories` (`category_id`, `title`, `description`) VALUES
+(1, 'Science Fiction', 'Science Fiction is the best cat ever'),
 (2, 'Humor', 'Humor'),
 (3, 'Horror', 'Horror'),
-(4, 'Western', 'Western');
+(4, 'Western', 'Western'),
+(5, 'Manga', 'Lorem manga ipsum mangas are for guurls'),
+(6, 'Drama', 'Dramanga ipsum dramas are for guurls');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories_copy`
+--
+
+DROP TABLE IF EXISTS `categories_copy`;
+CREATE TABLE IF NOT EXISTS `categories_copy` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(60) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories_copy`
+--
+
+INSERT INTO `categories_copy` (`category_id`, `title`, `description`) VALUES
+(1, 'Mangaaaa', 'Lorem manga ipsum mangas are for guurls'),
+(2, 'Mangaaaa', 'Lorem manga ipsum mangas are for guurls'),
+(3, 'Mangaaaa', 'Lorem manga ipsum mangas are for guurls'),
+(4, 'Mangaaaa', 'Lorem manga ipsum mangas are for guurls'),
+(5, 'Mangaaaa', 'Lorem manga ipsum mangas are for guurls'),
+(6, 'Mangaaaa', 'Lorem manga ipsum mangas are for guurls');
 
 -- --------------------------------------------------------
 
@@ -83,7 +111,7 @@ DROP TABLE IF EXISTS `movies`;
 CREATE TABLE IF NOT EXISTS `movies` (
   `movie_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(60) NOT NULL,
-  `description` varchar(200) NOT NULL,
+  `description` varchar(2000) NOT NULL,
   `picture` varchar(200) NOT NULL,
   `release_year` int(60) NOT NULL,
   `local_path` varchar(200) NOT NULL,
@@ -97,12 +125,12 @@ CREATE TABLE IF NOT EXISTS `movies` (
 --
 
 INSERT INTO `movies` (`movie_id`, `title`, `description`, `picture`, `release_year`, `local_path`, `category_id`) VALUES
-(3, 'final the bad and the good', 'bad bad bad bad bad bad bad bad bad', 'https://i0.wp.com/media.boingboing.net/wp-content/uploads/2016/07/gbu.jpg?w=800&ssl=1', 2021, 'c:/mymovies/olaf1.mp4', 2),
-(4, 'final monster madness', 'very scary stuff', 'https://cinemassacre.com/wp-content/uploads/2016/09/26-Rocky-Horror-190x140.jpg', 1979, 'c:/mymovies/olaf2.mov', 2),
-(5, 'finald teletubbies', 'not only for kids but it is real shit', 'https://i.ytimg.com/vi/W7wQJyED91I/maxresdefault.jpg', 2011, 'c:/mymovies/olaf3.mov', 4),
-(6, 'deep dive horizon 99 part 2', 'dive into it? really?', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP-bb-STMcVVKBgMvs9pg4Hnjf4aGOuwGx4QyIOlDlZIWJXfP2wA&s', 1999, 'c:/mymovies/olaf4.mov', 4),
-(7, 'break your neck badly to the bones part 3', 'it is not my fault!', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_SD4Fl4KSR9Oovr_soBpQZ8f1_rQ5Iapi2YwQHSSVUxDd6hy9mw&s', 2002, 'c:/mymovies/olaf5.mov', 4),
-(8, 'swampy swamp break up world part 4', 'boaaaahhhhrrrrrr', 'https://2.bp.blogspot.com/-UdoWFFJfhLA/W2jhhoc8NtI/AAAAAAAAYK8/MWLGOGeWdB092Aw39ALIt6Azntm1TTSRQCLcBGAs/s1600/Skanklodge.JPG', 2011, 'c:\\mymovies\\olaf6.mov', 2),
+(3, 'final the bad and the good', 'bad bad bad bad bad bad bad bad bad Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero obcaecati incidunt repellendus ex, facilis.', 'https://i0.wp.com/media.boingboing.net/wp-content/uploads/2016/07/gbu.jpg?w=800&ssl=1', 2021, 'c:/mymovies/olaf1.mp4', 2),
+(4, 'final monster madness', 'very scary stuffLorem ipsum dolor sit amet consectetur, adipisicing elit. Vero obcaecati incidunt repellendus ex, facilis.', 'https://cinemassacre.com/wp-content/uploads/2016/09/26-Rocky-Horror-190x140.jpg', 1979, 'c:/mymovies/olaf2.mov', 2),
+(5, 'finald teletubbies', 'not only for kids but it is real shitLorem ipsum dolor sit amet consectetur, adipisicing elit. Vero obcaecati incidunt repellendus ex, facilis.', 'https://i.ytimg.com/vi/W7wQJyED91I/maxresdefault.jpg', 2011, 'c:/mymovies/olaf3.mov', 4),
+(6, 'deep dive horizon 99 part 2', 'dive into it? really?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero obcaecati incidunt repellendus ex, facilis.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP-bb-STMcVVKBgMvs9pg4Hnjf4aGOuwGx4QyIOlDlZIWJXfP2wA&s', 1999, 'c:/mymovies/olaf4.mov', 4),
+(7, 'break your neck badly to the bones part 3', 'it is not my fault!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero obcaecati incidunt repellendus ex, facilis.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_SD4Fl4KSR9Oovr_soBpQZ8f1_rQ5Iapi2YwQHSSVUxDd6hy9mw&s', 2002, 'c:/mymovies/olaf5.mov', 4),
+(8, 'swampy swamp break up world part 4', 'boaaaahhhhrrrrrrLorem ipsum dolor sit amet consectetur, adipisicing elit. Vero obcaecati incidunt repellendus ex, facilis.', 'https://2.bp.blogspot.com/-UdoWFFJfhLA/W2jhhoc8NtI/AAAAAAAAYK8/MWLGOGeWdB092Aw39ALIt6Azntm1TTSRQCLcBGAs/s1600/Skanklodge.JPG', 2011, 'c:\\mymovies\\olaf6.mov', 2),
 (9, 'never thought that part 2', 'it is an empty mind', 'https://i.ytimg.com/vi/euknE24COWA/maxresdefault.jpg', 2015, 'c:\\mymovies\\olaf7.mov', 3),
 (10, 'fun size part 2', 'yes, just having fun', 'https://m.media-amazon.com/images/M/MV5BMjI1NDgyMjI4OV5BMl5BanBnXkFtZTcwNTg3MzM0OA@@._V1_.jpg', 2019, 'c:/mymovies/olaf8.mov', 1),
 (11, 'Passport part 2', 'very serious stuff', 'https://i.vimeocdn.com/video/765578324.jpg?mw=1920&mh=1080&q=70', 1995, 'c:/mymovies/olaf9.mov', 2),
@@ -155,12 +183,28 @@ CREATE TABLE IF NOT EXISTS `playlists_movies` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(60) NOT NULL,
-  `password` varchar(200) NOT NULL,
   `firstname` varchar(60) NOT NULL,
   `lastname` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(200) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `email`, `password`) VALUES
+(1, 'Reginald', 'Van', 'r@van.be', '$2y$10$q707YLx/FN0/ujfUPPaDteM4FME4/B1n06c1FLuJAbFPnbEzACAtW'),
+(2, 'Reginald2', 'Van2', 'r2@van.be', '$2y$10$4O/JpisKTybfDatPqe8aYOpJw9RKyTbJQa8ZGOrKKgjmAbm4q4chO'),
+(3, 'Reginald3', 'Van3', 'r3@van.be', '$2y$10$t40/p/R09j967WE4QqLpgu7EKi18afPsFHmRTLOc5..iniNe2PUDu'),
+(4, 'Reginald3', 'Van3', 'r3@van.be', '$2y$10$mWRqruiLzOOWG1hcOn.Vc..Z2WfF7eqEojv.G0gVGOYUKh1kw4BSO'),
+(5, 'Reginald3', 'Van3', 'r3@van.be', '$2y$10$7opurPrSvunLnezTwpcbx.EijOBjdZ7z/uelXqczWxExmeHT1rcSm'),
+(6, 'Reginald3', 'Van3', 'r3@van.be', '$2y$10$GsSKNN/7/wVJwBiKjf22V.XyPDraDWU9M5sOO/oLgVSN1rokMEsJG'),
+(7, 'Olaf', 'Olafname', 'olaf@amazon.com', '$2y$10$.ROFS6Ccu2iFHyt/TRHVoeegdnf8MBhMeosdrYY/rS6zbRnts2IQm'),
+(8, 'Olaf', 'Olafname', 'olaf2@amazon.com', '$2y$10$u4f0J0nyEm2qD8vutRJoM..cETQbbBwEBc7UVFQtwvQPQEvTzLK66'),
+(9, 'Olaf', 'Olafname', 'olaf2@oracle.com', '$2y$10$CYMejlK9uUtXhtUV/ooa9.9VZNPLRLLOgNcakvwVEuHFPSLJnnQaG'),
+(10, 'Reginald4', 'Van4', 'r4@van.be', '$2y$10$HSDO2x7dpAaRMLS1DBPaU.qjvMQPcZD83DCeZI8wxWHDrGgzQR1RS');
 
 --
 -- Constraints for dumped tables
@@ -177,7 +221,7 @@ ALTER TABLE `actors_movies`
 -- Constraints for table `movies`
 --
 ALTER TABLE `movies`
-  ADD CONSTRAINT `movies_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`categtory_id`);
+  ADD CONSTRAINT `movies_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
 
 --
 -- Constraints for table `playlists`
